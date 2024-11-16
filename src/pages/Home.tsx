e47cdf7898d50484e9a755e065833564ed2a585c
Ali <lord3cloud@gmail.com>
2024-11-17 03:32:28 +0500

    Commit on PC

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function EmptyPage() {
+  return (
+    <div className="min-h-screen bg-background/95 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-foreground">Главное</h1>
+    </div>
+  );
+}
